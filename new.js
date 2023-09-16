637fd02f69d8e07fd02b8ada1c6ab71a94cd0292
Aghar Al-Masri <aghar_4@hotmail.com>
2023-09-16 17:54:40 +0300

    Fix

--- new.js
+++ new.js
@@ -204,5 +204,5 @@ app.post('/submitAnswer', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://0.0.0.0:${port}`);
 });
