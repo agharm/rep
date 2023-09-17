7c676e1018f69cb1cde7c7691d86dba0a3b9f061
Aghar Al-Masri <aghar_4@hotmail.com>
2023-09-17 11:27:12 +0300

    Fix

--- new.js
+++ new.js
@@ -189,9 +189,10 @@ app.post('/submitAnswer', async (req, res) => {
         } else {
           const userEmail = row ? row.dbemail : '';
           // Send email with the admin's answer to the user
+          const { qtext, email } = req.body;//remove
           const userMailOptions = {
             from: config.gmail.user,
-            to: email,
+            to: email, //userEmail
             subject: 'Your Question Answered',
             text: 'Answer from Aghar:\n\n' + answer,
           };
