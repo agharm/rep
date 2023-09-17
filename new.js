0d9c045d4c1b57464bc94a65d2bc1f7b92f0cd89
Aghar Al-Masri <aghar_4@hotmail.com>
2023-09-17 13:57:42 +0300

    Fix

--- new.js
+++ new.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-const sqlite3 = require('sqlite3').verbose();
-const config = require('./config');
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
-const gmailUser = process.env.GMAIL_USER;
-const gmailPass = process.env.GMAIL_PASS;
+const { Pool } = require('pg');
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
@@ -17,81 +14,20 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// Create a new PostgreSQL pool
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // For development, you might need to disable SSL checks
+  },
+});
+
 // Serve the homepage
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/hom.html');
 });
 
-// Serve the question form
-app.get('/question-form', (req, res) => {
-  res.sendFile(__dirname + '/views/question-form.html');
-});
-
-app.get('/response', (req, res) => {
-  res.sendFile(__dirname + '/views/submitted.html');
-});
-
-app.get('/ted-talks', (req, res) => {
-  res.sendFile(__dirname + '/views/ted.html');
-});
-
-app.get('/projects', (req, res) => {
-  res.sendFile(__dirname + '/views/proj.html');
-});
-
-app.get('/cv', (req, res) => {
-  const cvFilePath = __dirname + '/docs/CV.pdf'; // Replace 'your_cv.pdf' with the actual file name
-
-  // Set the content type to PDF
-  res.setHeader('Content-Type', 'application/pdf');
-
-  // Provide options for downloading the file with a specific name
-  res.setHeader('Content-Disposition', 'attachment; filename=CV-Aghar Masri-Engineer & Software Developer.pdf');
-
-  // Send the CV file
-  res.sendFile(cvFilePath);
-});
-
-app.get('/word', (req, res) => {
-  const workFilePath = __dirname + '/docs/Work instructions.pdf'; // Replace 'your_cv.pdf' with the actual file name
-
-  // Set the content type to PDF
-  res.setHeader('Content-Type', 'application/pdf');
-
-  // Provide options for downloading the file with a specific name
-  res.setHeader('Content-Disposition', 'attachment; filename=Work instructions.pdf');
-
-  // Send the CV file
-  res.sendFile(workFilePath);
-});
-
-// Serve the Certificates page
-app.get('/certificates', (req, res) => {
-  // Set the path to your certificate.html file
-  const certificatesPagePath = path.join(__dirname, 'views', 'cert.html');
-
-  // Send the certificate.html file as a response
-  res.sendFile(certificatesPagePath);
-});
-
-// Create routes to serve individual certificates
-app.get('/certificates/:certificateName', (req, res) => {
-  // Get the certificate name from the URL parameters
-  const certificateName = req.params.certificateName;
-
-  // Set the path to the certificate file
-  const certificateFilePath = path.join(__dirname, 'docs', certificateName);
-
-  // Set the content type to PDF (or adjust to the appropriate content type)
-  res.setHeader('Content-Type', 'application/pdf');
-
-  // Provide options for downloading the file with a specific name
-  res.setHeader('Content-Disposition', `attachment; filename=${certificateName}`);
-
-  // Send the certificate file
-  res.sendFile(certificateFilePath);
-});
-
+// ... (Other routes)
 
 // Handle form submission
 app.post('/submit', async (req, res) => {
@@ -99,29 +35,21 @@ app.post('/submit', async (req, res) => {
   const questionId = uuidv4();
 
   // Store the question in the database
-  const db = new sqlite3.Database('./database/questions.db');
-  db.serialize(() => {
-    db.run(`
-      CREATE TABLE IF NOT EXISTS questions (
-        questionId TEXT PRIMARY KEY,
-        dbemail TEXT,
-        content TEXT,
-        dbanswer TEXT
-      )
-    `);
+  const insertQuery = 'INSERT INTO questions (questionId, dbemail, content) VALUES ($1, $2, $3)';
+  const values = [questionId, email, qtext];
 
-    const stmt = db.prepare('INSERT INTO questions (questionId, dbemail, content) VALUES (?, ?, ?)');
-    stmt.run(questionId, email, qtext);
-    stmt.finalize();
-  });
-  db.close();
+  try {
+    await pool.query(insertQuery, values);
+    console.log('Question inserted into the database');
+  } catch (error) {
+    console.error('Error inserting question into the database:', error);
+  }
 
   // Send email to admin with a link to the answer page
   const adminEmail = 'aghar_4@hotmail.com';
-  //const answerLink = `http://localhost:3000/answer?id=${questionId}`;
   const answerLink = `${req.protocol}://${req.get('host')}/answer?id=${questionId}`;
   const adminMailOptions = {
-    from: config.gmail.user,
+    from: process.env.GMAIL_USER, // Use the appropriate environment variable
     to: adminEmail,
     subject: email,
     text: `A new question has been submitted: ${qtext}\nAnswer it here: ${answerLink}`,
@@ -130,8 +58,8 @@ app.post('/submit', async (req, res) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: config.gmail.user,
-      pass: config.gmail.password,
+      user: process.env.GMAIL_USER, // Use the appropriate environment variable
+      pass: process.env.GMAIL_PASS, // Use the appropriate environment variable
     },
     tls: {
       rejectUnauthorized: false,
@@ -148,84 +76,8 @@ app.post('/submit', async (req, res) => {
   res.redirect('/response');
 });
 
-// Handle rendering the answer page for admin and processing answer submission
-app.get('/answer', (req, res) => {
-  const questionId = req.query.id;
-
-  // Open the database
-  const db = new sqlite3.Database('./database/questions.db');
-
-  // Retrieve the question text from the database using questionId
-  db.get('SELECT content FROM questions WHERE questionId = ?', [questionId], (err, row) => {
-    if (err) {
-      console.error('Error retrieving question text:', err);
-      res.status(500).send('Error retrieving question text');
-    } else {
-      const qtext = row ? row.content : 'Question not found';
-      res.render('answerform', { qText: qtext, questionId: questionId });
-    }
-  });
-
-  // Close the database
-  db.close();
-});
-
-// Handle the form submission for answers
-app.post('/submitAnswer', async (req, res) => {
-  const { answer, questionId } = req.body;
-
-  // Update the database with the answer
-  const db = new sqlite3.Database('./database/questions.db');
-  db.run('UPDATE questions SET dbanswer = ? WHERE questionId = ?', [answer, questionId], (updateErr) => {
-    if (updateErr) {
-      console.error('Error inserting answer:', updateErr);
-      res.status(500).send('Error inserting answer');
-    } else {
-      // Retrieve the user's email from the database
-      db.get('SELECT dbemail FROM questions WHERE questionId = ?', [questionId], async (emailErr, row) => {
-        if (emailErr) {
-          console.error('Error retrieving user email:', emailErr);
-          res.status(500).send('Error retrieving user email');
-        } else {
-          const userEmail = row ? row.dbemail : '';
-          // Send email with the admin's answer to the user
-          const userMailOptions = {
-            from: config.gmail.user,
-            to: userEmail, //email
-            subject: 'Your Question Answered',
-            text: 'Answer from Aghar:\n\n' + answer,
-          };
-
-          const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-              user: config.gmail.user,
-              pass: config.gmail.password,
-            },
-            tls: {
-              rejectUnauthorized: false,
-            },
-          });
-
-          try {
-            await transporter.sendMail(userMailOptions);
-            console.log('Email to user sent successfully');
-          } catch (userEmailErr) {
-            console.error('Error sending email to user:', userEmailErr);
-          }
-          res.send('Email sent successfully');
-        }
-      });
-    }
-  });
-
-  // Close the database
-  db.close();
-});
-
-//app.listen(process.env.PORT || 3000)
+// ... (Other routes)
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
